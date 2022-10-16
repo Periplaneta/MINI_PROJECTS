672507aea2640de762ce9ca4011b650c570da1fb
Enimil Isaac Obo <90509124+ioenimil@users.noreply.github.com>
2022-10-16 19:33:28 +0000

    Update reversal_algorithm.py
    Optimized the swapping process by using simultaneous assignment of list elements

--- reversal_algorithm.py
+++ reversal_algorithm.py
@@ -9,29 +9,29 @@ linkedIn:linkedin.com/in/isaac-obo-enimil-2a2354223
 
 ----------------------------------------------------
 This reversal algorithm reverses every sequence that
-will be passed as argument to the function
+is passed as an argument to the function.
 --------------------------------------------------
 '''
 
 
-# Define a reversal function
+# Define a reversal function.
 def reversal(sequence):
     # Assigning pointers
     tail_pointer = len(sequence) - 1
     head_pointer = 0
 
-    # Checking if the tail_pointer is greater than the head_pointer
+    # Checking if the tail_pointer is greater than the head_pointer.
     while head_pointer < tail_pointer:
 
         # Swapping elements in the list
-        temp = sequence[head_pointer]
-        sequence[head_pointer] = sequence[tail_pointer]
-        sequence[tail_pointer] = temp
-        # changing the location of the pointers
+        sequence[head_pointer], sequence[tail_pointer] = sequence[tail_pointer], sequence[head_pointer]
+       
+        # changing the location of the pointers.
         tail_pointer -= 1
         head_pointer += 1
 
-    # Returning the modified sequence
+    # Returning the modified sequence.
     return sequence
+
 
 
